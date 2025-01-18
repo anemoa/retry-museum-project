1417a35e072461055b3fba20e66a93903ee1ff8d
anemoa <anemoia.yves@gmail.com>
2025-01-18 23:55:55 +0900

    style: add ObjDetailSection

--- src/pages/work-of-arts/components/ObjectDetailSection.jsx
+++ src/pages/work-of-arts/components/ObjectDetailSection.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const DetailSection = styled.section`
+const ObjDetailSection = styled.section`
     display: flex;
     background-color: #f9f9f9;
 `;
 
 export const ObjectDetailSection = () => {
     return (
-        <section>
+        <ObjDetailSection>
             <div className='arts_pic_wrap'>
                 <div className='art_pic_box'>
                     <img src='' alt='' />
                 </div>
 
                 <div className='art_simple_info'>
-                    <h2 className='art_title'></h2>
+                    <h2 className='art_title'>ㅇㅇㅇ</h2>
 
                     <div className='art_info_box'>
                         <strong className='info_title'>Maker</strong>
-                        <p className='info_value'></p>
+                        <p className='info_value'>ㄴㄴㄴ</p>
                     </div>
 
                     <div className='art_info_box'>
                         <strong className='info_title'>Production date</strong>
-                        <p className='info_value'></p>
+                        <p className='info_value'>ㅇㅇㄴㄴ</p>
                     </div>
                 </div>
             </div>
-        </section>
+        </ObjDetailSection>
     );
 };
