7431c37f9777e3cfb616ef96f5c5785e64ac2721
anemoa <anemoia.yves@gmail.com>
2025-01-27 20:21:38 +0900

    feat: add ObjInfoSection style and structure

--- src/pages/work-of-arts/components/ObjectInfoSection.jsx
+++ src/pages/work-of-arts/components/ObjectInfoSection.jsx
@@ -1,13 +1,66 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
-const ObjInfoSection = styled.section``;
+const ObjInfoSection = styled.section`
+    display: flex;
+    background-color: #eee;
+`;
+
+const ObjImageBox = styled.div`
+    width: 50%;
+    max-width: 750px;
+
+	img{
+		width: 100%;
+	}
+`;
+
+
+const ObjArtInfoBox = styled.div`
+    width: 50%;
+    padding: 1rem 1.5625rem;
+	text-align: left;
+
+	h2{
+		color: limegreen;
+	}
+`;
+
+const ObjArtInfoElement = styled.div`
+	margin-top: 1.5rem;
+
+	strong{
+		font-size: 26px;
+		font-weight: bold;
+	}
+
+	p{
+		margin-top: 5px;
+		font-size: 16px;
+		color: red;
+	}
+`;
 
 export const ObjectInfoSection = () => {
-  return (
-	<section>
-		<div>img box</div>
-		<div>art info box</div>
-	</section>
-  )
-}
+    return (
+        <ObjInfoSection>
+            <ObjImageBox>
+                <img src='' alt='' />
+            </ObjImageBox>
+
+            <ObjArtInfoBox>
+                <h2>title</h2>
+
+                <ObjArtInfoElement>
+                    <strong>Maker</strong>
+                    <p>artist</p>
+                </ObjArtInfoElement>
+
+                <ObjArtInfoElement>
+                    <strong>Production date</strong>
+                    <p>date</p>
+                </ObjArtInfoElement>
+            </ObjArtInfoBox>
+        </ObjInfoSection>
+    );
+};
